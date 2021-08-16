3ae53458975e0b24e679716ae5a5695685f66426
Paweł Krawczyk <pawel.krawczyk@interia.com>
2021-08-16 15:51:40 +0200

    Update settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.DEPLOY_URL ? 'https://proster-project.herokuapp.com/' : `http://localhost:1337`,
         queryLimit: 1000, // Defaults to 100
         collectionTypes:[
           'jobs',
